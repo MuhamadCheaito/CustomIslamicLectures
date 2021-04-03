9cb90e3f8fe3fbed8242e04007339a3c1610726a
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2021-04-03 18:16:25 +0300

    Add project files.

---
+++ IslamicConfPlayer/frmMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace IslamicConfPlayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panHeader = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panVideos = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panContent = new System.Windows.Forms.Panel();
+            this.videoContent = new System.Windows.Forms.WebBrowser();
+            this.panHeader.SuspendLayout();
+            this.panVideos.SuspendLayout();
+            this.panContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panHeader
+            // 
+            this.panHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(50)))), ((int)(((byte)(56)))));
+            this.panHeader.Controls.Add(this.btnMinimize);
+            this.panHeader.Controls.Add(this.btnClose);
+            this.panHeader.Controls.Add(this.lblTitle);
+            this.panHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panHeader.ForeColor = System.Drawing.Color.White;
+            this.panHeader.Location = new System.Drawing.Point(3, 3);
+            this.panHeader.Name = "panHeader";
+            this.panHeader.Size = new System.Drawing.Size(1194, 58);
+            this.panHeader.TabIndex = 0;
+            this.panHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.headerPanel_MouseMove);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.Location = new System.Drawing.Point(1120, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(37, 58);
+            this.btnMinimize.TabIndex = 2;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(1157, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(37, 58);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.lblTitle.Location = new System.Drawing.Point(481, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(379, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "محاضرات مختارة للسيد محمد حسين فضل الله";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseMove);
+            // 
+            // panVideos
+            // 
+            this.panVideos.Controls.Add(this.listView1);
+            this.panVideos.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panVideos.Location = new System.Drawing.Point(3, 61);
+            this.panVideos.Name = "panVideos";
+            this.panVideos.Size = new System.Drawing.Size(296, 636);
+            this.panVideos.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Times New Roman", 13F);
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listView1.RightToLeftLayout = true;
+            this.listView1.Size = new System.Drawing.Size(296, 636);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.TileSize = new System.Drawing.Size(228, 30);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // panContent
+            // 
+            this.panContent.Controls.Add(this.videoContent);
+            this.panContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panContent.Location = new System.Drawing.Point(299, 61);
+            this.panContent.Name = "panContent";
+            this.panContent.Size = new System.Drawing.Size(898, 636);
+            this.panContent.TabIndex = 2;
+            // 
+            // videoContent
+            // 
+            this.videoContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoContent.Location = new System.Drawing.Point(0, 0);
+            this.videoContent.MinimumSize = new System.Drawing.Size(20, 20);
+            this.videoContent.Name = "videoContent";
+            this.videoContent.Size = new System.Drawing.Size(898, 636);
+            this.videoContent.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.ControlBox = false;
+            this.Controls.Add(this.panContent);
+            this.Controls.Add(this.panVideos);
+            this.Controls.Add(this.panHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "frmMain";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panHeader.ResumeLayout(false);
+            this.panHeader.PerformLayout();
+            this.panVideos.ResumeLayout(false);
+            this.panContent.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panVideos;
+        private System.Windows.Forms.Panel panContent;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.WebBrowser videoContent;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ IslamicConfPlayer/frmMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IslamicConfPlayer
+{
+    public partial class frmMain : MaterialForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        string VideoId;
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void headerPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void lblTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            VideoId = e.Item.ToolTipText.Trim();
+            var embed = "<html><head>" +
+            "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\"/>" +
+            "</head><body style=\"background-color:black;\">" +
+            "<iframe width=\"" + (videoContent.Width - 25) + "\" height=\"" + (videoContent.Height - 25) + "\" src=\"{0}\"" +
+            "frameborder = \"0\" allow = \"autoplay; encrypted-media\" allowfullscreen></iframe>" +
+            "</body></html>";
+            var url = $"https://www.youtube.com/embed/{VideoId}?rel=0&autoplay=1";
+            this.videoContent.DocumentText = string.Format(embed, url);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            JObject videos = JObject.Parse(File.ReadAllText(@".\videos.json"));
+            foreach(var video in videos["playlist"]["videos"])
+            {
+                ListViewItem item = new ListViewItem();
+                item.ToolTipText = video["id"].ToString();
+                item.Text = video["name"].ToString();
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
